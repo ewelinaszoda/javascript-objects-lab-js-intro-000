45a3327e0569c5b5b058a756ec1e36ce175860f8
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-03-30 17:15:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, object, {[key]: value});
+  var cloneObj = Object.assign({}, object, {[key]: value});
+  cloneObj[key] = value;
+  return cloneObj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
+  return object;
 }
+
+
+
